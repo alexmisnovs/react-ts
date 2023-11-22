235811a6bb36f6aad0c9279d1a7c901cf848fca9
Alex macbookAir <aleksandrmisnov@gmail.com>
2023-11-22 17:06:07 +0000

    added blog posts

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,44 @@ import TimersContextProvider from "./store/timers-store";
 import Header from "./components/Header";
 import Timers from "./components/Timers";
 import AddTimer from "./components/AddTimer";
+import { get } from "./utils/http";
+import { ReactNode, useEffect, useState } from "react";
+import BlogPosts, { type BlogPost } from "./components/BlogPosts";
+
+type RawDataBlogPost = {
+  id: number;
+  userId: number;
+  title: string;
+  body: string;
+};
 
 function App() {
+  const [fetchedPosts, setFetchedPosts] = useState<BlogPost[] | undefined>();
+
+  useEffect(() => {
+    async function fetchPosts() {
+      const data = (await get("https://jsonplaceholder.typicode.com/posts")) as RawDataBlogPost[];
+
+      const blogPosts: BlogPost[] = data.map(rawPost => {
+        return {
+          id: rawPost.id,
+          title: rawPost.title,
+          text: rawPost.body,
+        };
+      });
+
+      setFetchedPosts(blogPosts);
+    }
+
+    fetchPosts();
+  }, []);
+
+  let content: ReactNode;
+
+  if (fetchedPosts) {
+    content = <BlogPosts posts={fetchedPosts} />;
+  }
+
   return (
     <TimersContextProvider>
       <Header />
@@ -12,6 +48,7 @@ function App() {
         <AddTimer />
         <Timers />
       </main>
+      {content}
     </TimersContextProvider>
   );
 }
